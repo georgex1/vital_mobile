3f295d1711f8e4f683f40749d11691d531f5845c
georgex1 <grandgeorgex@hotmail.com>
2014-03-31 19:35:19 -0300

    1

--- js/functions.js
+++ js/functions.js
@@ -24,12 +24,12 @@ if(isPhonegap){
     });
 }
 
-$(document).on('pagebeforechange', function(e, data){
+/*$(document).on('pagebeforechange', function(e, data){
     $('#inactiveScreen2').show();
     setTimeout(function(){
         $('#inactiveScreen2').hide();
     }, 200);
-});
+});*/
 
 $(document).on('pagechange', function(e, data){
     /*$('#inactiveScreen2').show();
